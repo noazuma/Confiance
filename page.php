4e5cd77cfa639a442f5454785b0cacee2d0ca698
kazuki higashi <kazukihigashi@kazukinoMacBook-Air.local>
2022-11-27 17:14:49 +0900

    お問い合わせ

--- page.php
+++ page.php
@@ -1,52 +1,31 @@
 <?php get_header(); ?>
-<sidebar class="l-sidebar">
-        
-        <button class="c-button">
-            <span class="c-line c-button__br"></span>
-            <span class="c-line c-button__br"></span>
-            <span class="c-line c-button__br"></span>
-            </button>
-        <nav class="p-menu">
-        <ul class="u-padding10"><a href="#" class="c-text">事業内容</a></ul>
-        <ul class="u-padding10"><a href="#" class="c-text">会社概要</a></ul>
-        <ul class="u-padding10"><a href="#" class="c-text">お問合せ</a></ul>
-    <!-- </div> -->
-        </nav>
-</sidebar>
+
 <!-- トップ画面 -->
     <main class="l-main">
         <div class="p-front__top">
-            <h1 class="p-front__title c-text c-shadow">障がい福祉コンサルティング</h1>
+        <h1 class="p-front__title c-text c-shadow"><?php bloginfo( 'description' ); ?></h1>
             <P class="c-text c-shadow">テキストが入ります。テキストが入ります。</P>
         </div>
+        <?php
+    // if(have_posts()):  
+    //     while(have_posts()):
+    //         the_post(); ?>
         <div class="p-box--menu u-padding20">
-            <div class="p-box--menu__open u-margin20 u-font-size__main u-padding__5 c-text c-shadow">
-                事業開業・開設支援
+            <div class="u-margin20 u-font-size__main u-padding__5 c-text c-shadow">
+            <?php if(has_post_thumbnail()): ?>
+                <?php the_post_thumbnail('full',array('class' => 'p-box--menu__open')); ?>
+        <?php else: ?>
+            <?php endif; ?>
             </div>
         </div>
         <div class="p-box--text">
             <ul class="p-box">
                 </li>
                 <li class="p-box--news__group">
-                    <a href="#">
-                        <p class="u-font-size">ここにお知らせが入りますここにお知らせが入りますここにお知らせが入ります
-                            ここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入ります
-                            ここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入ります
-                            ここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入ります
-                            ここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入りますここにお知らせが入ります
-                        </p>
+                        <p class="u-font-size"><?php the_content(); ?>
                     </a>
                 </li>
             </ul>
         </div>
     </main>
-    <footer class="l-footer p-footer">
-
-        <p class="c-position" a href="#">プライバシーポリシー</p>
-        <p class="c-position c-position__70" a href="#">Copyright © Confiance Ltd.</p>
-    </footer>
-</div>
-<script src="/js/jquery-3.6.1.min.js"></script>
-<script src="/js/script.js"></script>
-</body>
-</html>
+<?php get_footer(); ?>
